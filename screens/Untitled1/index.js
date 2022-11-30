8b4aff3ea4522f2fcc225cd944eab7ca3c07a5d5
Brian LeVee <brianlevee@gmail.com>
2022-11-30 18:30:45 -0500

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,25 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.vrHaeYGN}>
+      <TextInput style={_styles.znaOGogl}></TextInput></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  vrHaeYGN: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  znaOGogl: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  }
+});
